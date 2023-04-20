6b7a811e7b6adf7d589b65aa020ca0af2368656c
Ilmira Sitdikova <ilmira.sitdikova.1991@mail.ru>
2023-04-20 13:47:37 +0300

    Массив из 8 элементов

---
+++ Task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int i;
+int[] a = new int[8];
+Console.WriteLine("Введите массив чисел: ");
+for (i = 0; i < 8; i++)
+{
+    a[i] = int.Parse(Console.ReadLine()?? " ");
+}
+for (i = 0; i < 8; i++)
+{
+    Console.Write($"{a[i]} | " );
+}
+
+
