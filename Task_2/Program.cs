32007e21e55a8bb5729dd9d064ea55beb29d154f
Ilmira Sitdikova <ilmira.sitdikova.1991@mail.ru>
2023-04-20 12:58:40 +0300

    Сумма цифр в числе

---
+++ Task_2/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine() ?? " ");
+int res = 0;
+while (num != 0)
+{
+    res += num % 10;
+    num /= 10;
+}
+Console.WriteLine($"Сумма цифр в числе: {res} ");
